b6088dfc5658ef745eadcf311dd360c4ccf57ed3
nasustim <mitsuhibino@gmail.com>
2022-12-03 14:04:35 +0900

    test(go): cmd/cli/main.go

---
+++ cmd/cli/main_export.go
@@ -0,0 +1,5 @@
+package main
+
+func Run(tmplPath string, output string) int {
+	return run(tmplPath, output)
+}
---
+++ cmd/cli/main_test.go
@@ -0,0 +1,16 @@
+package main_test
+
+import (
+	"testing"
+
+	cli "github.com/nasustim/nasustim/cmd/cli"
+)
+
+func Test_Run(t *testing.T) {
+	t.Run("exit successfully", func(t *testing.T) {
+		actual := cli.Run("../../tmpl/README.gomd", "../../README.md")
+		if actual != cli.EXIT_SUCCESS {
+			t.Error("unexpected result")
+		}
+	})
+}
